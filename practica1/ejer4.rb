83f0aec90a118f35befbdee7b149b383cbb77b23
Facundo Pomar <facundopomar@hotmail.com>
2015-08-31 20:55:35 -0300

    Some changes needed

--- practica1/ejer4.rb
+++ practica1/ejer4.rb
@@ -1,7 +1,9 @@
 #Devolver info sobre la hora
-def hora_actual
+#Rehacer con case
+#Meter sumar una hora en otera fucnion
+def hora_actual(aHour)
 
-	min = Time.now.min
+	min = aHour.min
 
 	if (0..10).include?(min)
 		return "En punto"
@@ -10,13 +12,13 @@ def hora_actual
 	elsif (21..34).include?(min)
 		return "y media"
 	elsif (35..44).include?(min)
-		return "#{(Time.now + (60*60)).hour} menos veiticinco"
+		return "#{(aHour + (60*60)).hour} menos veiticinco"
 	elsif (45..55).include?(min)
-		return "#{(Time.now + (60*60)).hour} menos cuarto"
+		return "#{(aHour + (60*60)).hour} menos cuarto"
 	else
-		return "casi las #{(Time.now + (60*60)).hour}"	
+		return "casi las #{(aHour + (60*60)).hour}"	
 	end
 end
 
-puts hora_actual
+puts hora_actual Time.now
 
