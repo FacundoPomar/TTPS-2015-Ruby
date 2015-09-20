4bf7accaa1051e918a47ef77749b439a257a73ce
Facundo Pomar <facundopomar@hotmail.com>
2015-09-20 15:21:36 -0300

    Added ejer8

---
+++ practica1/ejer8.rb
@@ -0,0 +1,7 @@
+#8. ¿Qué hace el siguiente código?
+
+#[:upcase, :downcase, :capitalize, :swapcase].map do |meth|
+#     "TTPS Opción Ruby".send(meth)
+#   end
+
+#El código enviar ĺlama con send, los métodos :upcase, :downcase, :capitalize y :swapcase al string "TTPS Optión Ruby"
