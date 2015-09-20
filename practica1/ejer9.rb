aafc2321f6e244504ba84e6ff0b48bc33deb827e
Facundo Pomar <facundopomar@hotmail.com>
2015-09-20 15:28:47 -0300

    Added ejer9

---
+++ practica1/ejer9.rb
@@ -0,0 +1,14 @@
+# 9. Escribí una función que dado un arreglo que contenga varios `string` cualesquiera, retorne un nuevo arreglo donde
+#    cada elemento es la longitud del `string` que se encuentra en la misma posición del arreglo recibido como parámetro.
+#    Por ejemplo:
+
+#      ```ruby
+#     longitud(['TTPS', 'Opción', 'Ruby', 'Cursada 2015'])
+#     # => [4, 6, 4, 12]
+#     ```
+
+def longitud(arr)
+	result = []
+	arr.each { |elem| result << elem.size}
+	result
+end
