b3f3ecb2ad6008e728ef227f998e83603e162a2c
FacundoPomar <facundopomar@hotmail.com>
2015-09-20 19:24:16 -0300

    Added ejer01

---
+++ practica2/ejer01.rb
@@ -0,0 +1,11 @@
+# 1. Implementá un método que reciba como parámetro un arreglo de números, los ordene y devuelva el resultado. Por
+#    ejemplo:
+
+#    ```ruby
+#    ordenar_arreglo([1, 4, 6, 2, 3, 0, 10])
+#    # => [0, 1, 2, 3, 4, 6, 10]
+#    ```
+
+def ordernar_arreglo(arr)
+	arr.sort
+end
