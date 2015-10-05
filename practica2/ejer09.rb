c7743eef9a132a1fca53b992729d8ef2f2164e63
FacundoPomar <facundopomar@hotmail.com>
2015-10-05 20:06:32 -0300

    Added ejer09

---
+++ practica2/ejer09.rb
@@ -0,0 +1,9 @@
+# 9. ¿Qué diferencia hay entre el uso de `include` y `extend` a la hora de incorporar un módulo en una clase?
+#   1. Si quisieras usar un módulo para agregar métodos de instancia a una clase, ¿qué forma usarías a la hora de
+#      incluirlo en la clase?
+#   2. Si en cambio quisieras usar un módulo para agregar métodos de clase, ¿qué forma usarías a la hora de incluir el
+#      módulo en la clase?
+
+El include sirve para agregar los métodos del módulo, como métodos de instancia.
+
+Mientras que el extend agrega los métodos como métodos de clase.
