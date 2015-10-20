1f2c4bca143bfd43d1c22b4ef02b135d6237a1d2
Facundo Pomar <facundopomar@hotmail.com>
2015-10-20 10:58:06 -0300

    Added ejer_5 of modules and classes

---
+++ practica2/ejer_mod5.rb
@@ -0,0 +1,24 @@
+# Dada la siguiente clase abstracta GenericFactory , implementá subclases de la misma que
+# permitan la creación de instancias de dichas clases mediante el uso del método de clase .create ,
+# de manera tal que luego puedas usar esa lógica para instanciar objetos sin invocar directamente el
+# constructor new .
+
+
+class GenericFactory
+	def self.create(**args)
+		new(**args)
+	end
+	def initialize(**args)
+		raise NotImplementedError
+	end
+end
+
+class Persona < GenericFactory
+	attr_accessor(:nombre)
+
+	def initialize(**args)
+		@nombre = args[:nombre]
+	end
+end
+
+p Persona.create({nombre: "Pedro GOnzalez"})
